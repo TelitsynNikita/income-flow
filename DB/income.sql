4190f0c9530c3ac3be8fff258b02cb1fc4654993
telitsyn nikita <telicyn.nikita@wb.ru>
2025-11-22 11:01:46 +0300

    add table income

---
+++ DB/income.sql
@@ -0,0 +1,8 @@
+CREATE TABLE income (
+    id SERIAL PRIMARY KEY ,
+    goods_id INTEGER NOT NULL ,
+    section_id INTEGER NOT NULL ,
+    goods_count SMALLINT NOT NULL ,
+    FOREIGN KEY (goods_id) REFERENCES goods (id),
+    FOREIGN KEY (section_id) REFERENCES sections (id)
+);
