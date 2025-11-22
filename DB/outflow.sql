ab6cb75c85dacc113e8f1635a8a2a21abcdfc87a
telitsyn nikita <telicyn.nikita@wb.ru>
2025-11-22 12:05:43 +0300

    add contractors id to outflow table

--- DB/outflow.sql
+++ DB/outflow.sql
@@ -2,5 +2,7 @@ CREATE TABLE outflow (
     id SERIAL PRIMARY KEY ,
     goods_id INTEGER NOT NULL ,
     goods_count SMALLINT NOT NULL ,
-    FOREIGN KEY (goods_id) REFERENCES goods (id)
+    contractors_id INTEGER NOT NULL ,
+    FOREIGN KEY (goods_id) REFERENCES goods (id) ,
+    FOREIGN KEY (contractors_id) REFERENCES contractors (id)
 );
